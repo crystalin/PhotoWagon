7d962f7d441656f174ad0723cd6b82fb6f1487e9
unknown <Crystalin@.(none)>
2011-03-31 10:20:17 +0900

    Initial Push (Rails 3 Skeleton)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photowagon::Application.config.secret_token = 'bf10e882d20c332fa1d82cb4453a7cf371fc14bada192f2ae635732f7f656ab7cac2e9d1123cfda4017d19728923dbedb34490b92be7fcbd78658ca5f8363bed'
