cfa652fb0afa0ace36c3651ba3373369ded0262c
Alan Sapede <salan@google.com>
2012-03-16 17:19:25 +0100

    Added Jcrop

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,3 +1,41 @@
 ActiveAdmin.register Post do
-  
+
+  #index :as => :grid do |post|
+  #  link_to(image_tag(post.image_url(:front_page)), admin_post_path(post))
+  #end
+
+  index :as => :block do |post|
+    div :class => 'post_image', :for => post do
+      link_to(image_tag(post.image_url(:front_page)), admin_post_path(post))
+    end
+  end
+
+  show do
+    h3 post.title
+    div do
+      post.body
+    end
+    div :class => 'preview' do
+      image_tag(post.image_url)
+    end
+    div do
+      image_tag(post.image_url(:cover))
+    end
+    div do
+      image_tag(post.image_url, :id => 'cropbox')
+    end
+  end
+
+  member_action :read_information do
+    post = Post.find(params[:id])
+    post.read_information
+    post.save!
+    redirect_to :action => :show, :notice => "Information read and save"
+  end
+
+  action_item :only => :show do
+    link_to "Update IPTC data", read_information_admin_post_path(post), :confirm => "Description and Title will be replaced.\n\nAre you sure?"
+  end
+
+
 end
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,32 @@
 //= require active_admin/base
+//= require jquery.Jcrop
+
+
+$(function() {
+    $('#cropbox').Jcrop({
+        onChange: update_crop,
+        onSelect: update_crop,
+        setSelect: [0, 0, 1600, 896],
+        addClass: 'jcrop-light',
+        aspectRatio: 300/ 168
+    });
+});
+
+function update_crop(coords) {
+    var rx = 300/coords.w;
+    var ry = 168/coords.h;
+    var lw = $('#cropbox').width();
+    var lh = $('#cropbox').height();
+    var ratio = 300.0 / lw ;
+
+    $('.preview img').css({
+      width: Math.round(rx * lw) + 'px',
+      height: Math.round(ry * lh) + 'px',
+      marginLeft: '-' + Math.round(rx * coords.x) + 'px',
+      marginTop: '-' + Math.round(ry * coords.y) + 'px'
+    });
+//  $("#crop_x").val(Math.round(coords.x * ratio));
+//  $("#crop_y").val(Math.round(coords.y * ratio));
+//  $("#crop_w").val(Math.round(coords.w * ratio));
+//  $("#crop_h").val(Math.round(coords.h * ratio));
+}
